231ac9ac7f8b86111fab144775175660a1e0f39c
DannyZaca84 <d_lunaroja@hotmail.co>
2019-12-29 22:49:51 -0800

    Section Two, Video Two

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -2,6 +2,9 @@ package com.example.msgshareapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,7 +12,17 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)//
 
+        btnShowToast.setOnClickListener {
+            //Code
+            Log.i(
+                "MainActivity",
+                "Button was clicked My Friend !"
+            ) //This statement is to specify the functionality of the code, it will register onto the LogCat Tab
+
+            //toast Statement
+            Toast.makeText(this, "Button Was Clicked !", Toast.LENGTH_SHORT).show()
 
 
+        }
     }
 }
