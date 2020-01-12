51bcf12d4c3242af1599b0bf5aa093e6b697193f
DannyZaca84 <d_lunaroja@hotmail.co>
2020-01-12 00:49:24 -0800

    Section Two, Video Four

--- app/src/main/java/com/example/msgshareapp/MainActivity.kt
+++ app/src/main/java/com/example/msgshareapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.msgshareapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
@@ -14,6 +16,8 @@ class MainActivity : AppCompatActivity() {
 
         btnShowToast.setOnClickListener {
             //Code
+            //this is an example on how to register user interaction or activity with the application into the logcat option.
+            //it registers any type of interaction with the application.
             Log.i(
                 "MainActivity",
                 "Button was clicked My Friend !"
@@ -24,5 +28,41 @@ class MainActivity : AppCompatActivity() {
 
 
         }
+
+        btnSendMsgToNextActivity.setOnClickListener {
+
+
+            //the concept of get/set has change and now is used as the syntax variable, in this case etUserMessage then just "text" toString.
+            // and this is how we parse / get the information from the user using the variable etUserMessage
+            val message: String = etUserMessage.text.toString()
+            //now, create a new activity so we can navigate from one to another.
+
+            //Intent can be interpreted as the "Intention of doing something"
+            //in this case we are intending to navigate to another activity
+
+            //define intent
+            //note: when "this" is present, it refers to the content of "THIS" activity
+
+            //concept of kotlin reflexion: ::class.java
+            //https://kotlinlang.org/docs/reference/reflection.html
+            val intent = Intent(this, SecondActivity::class.java)
+            startActivity(intent)//This is an example of explicit intent
+
+            intent.putExtra("user_message",message)
+
+            startActivity(intent)
+
+
+            //don't forget to declare activity into the AndroidManifest.xml
+
+            //if application crashes, refer to logcat to identify problem
+
+
+
+
+        }
+
+
+
     }
 }
---
+++ app/src/main/java/com/example/msgshareapp/SecondActivity.kt
@@ -0,0 +1,33 @@
+package com.example.msgshareapp
+
+import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_second.*
+
+class SecondActivity : AppCompatActivity(){
+
+    //type onCreate and select the second parameter with only of saveInstanceState
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_second)
+
+        //this is how to extract the incoming value from the user_message coming from main activity
+     val bundle: Bundle? = intent.extras
+        //Bundle? is a nullable
+
+        val msg = bundle!!.getString("user_message")
+        //now get the key from the main activity, it must match.
+        // "!!" not null assertion operator
+
+        Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        txvUserMessage.text = msg
+
+
+
+
+
+    }
+
+
+}
