c66ac75cb5c2bfb62dbc07b50cc1b2a8c26bebbc
DannyZaca84 <d_lunaroja@hotmail.co>
2020-01-17 00:27:51 -0800

    Section Three, Video Three

---
+++ app/src/main/java/com/example/msgshareapp/Model.kt
@@ -0,0 +1,46 @@
+package com.example.msgshareapp
+
+data class Hobby(var title: String)
+
+
+object Supplier{
+
+    //                           This is how we populate the list
+    val hobbies = listOf<Hobby>(
+        Hobby("Reading"),
+        Hobby("Walk"),
+        Hobby("Kick Box"),
+        Hobby("Computers"),
+        Hobby("Video Games"),
+        Hobby("Gym"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walk"),
+        Hobby("Kick Box"),
+        Hobby("Computers"),
+        Hobby("Video Games"),
+        Hobby("Gym"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walk"),
+        Hobby("Kick Box"),
+        Hobby("Computers"),
+        Hobby("Video Games"),
+        Hobby("Gym"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walk"),
+        Hobby("Kick Box"),
+        Hobby("Computers"),
+        Hobby("Video Games"),
+        Hobby("Gym"),
+        Hobby("Swimming"),
+        Hobby("Reading"),
+        Hobby("Walk"),
+        Hobby("Kick Box"),
+        Hobby("Computers"),
+        Hobby("Video Games"),
+        Hobby("Gym"),
+        Hobby("Swimming")
+    )//end of val hobbies
+}
